0a83e386c4b6772327cbea36e51569ab58bfe57d
johnconstantinides <johnconstantinides02@example.com>
2021-08-06 02:20:47 -0400

    did stuff

--- helper.py
+++ helper.py
@@ -4,11 +4,11 @@ from typing import List
 def print_board(board) -> str:
     line = f"""
     {board[0]}{board[1]}{board[2]}{board[3]}{board[4]}{board[5]}{board[6]}
-    {board[7]}{board[8]}{board[9]}{board[10]}{board[11]}{board[12]}{board[13]}
-    {board[14]}{board[15]}{board[16]}{board[17]}{board[18]}{board[19]}{board[20]}
-    {board[21]}{board[22]}{board[23]}{board[24]}{board[25]}{board[26]}{board[27]}
-    {board[28]}{board[29]}{board[30]}{board[31]}{board[32]}{board[33]}{board[34]}
-    {board[35]}{board[36]}{board[37]}{board[38]}{board[39]}{board[40]}{board[41]}
+{board[7]}{board[8]}{board[9]}{board[10]}{board[11]}{board[12]}{board[13]}
+{board[14]}{board[15]}{board[16]}{board[17]}{board[18]}{board[19]}{board[20]}
+{board[21]}{board[22]}{board[23]}{board[24]}{board[25]}{board[26]}{board[27]}
+{board[28]}{board[29]}{board[30]}{board[31]}{board[32]}{board[33]}{board[34]}
+{board[35]}{board[36]}{board[37]}{board[38]}{board[39]}{board[40]}{board[41]}
     """
     return line
 
@@ -24,11 +24,9 @@ def has_won(board,place,piece) -> bool:
     column = place // 7
     column = column * 7
     for i in range(4):
-        print(board[i + column],board[i + column+1],board[i + column+2],board[i + column+3])
         if board[i + column] == piece and board[i + column + 1] == piece and board[i + column + 2] == piece and board[i + column +3] == piece:
                 return True
     
     #diagonal check
-    
 
     return False
